5432e27982f65f86f3e0ccee751434c6e204b9fd
evrenkaraarslan <evrenkaraarslan@hotmail.com>
2020-05-20 16:01:51 +0200

    React Jammming

---
+++ src/Components/PlayListtt/PlayListtt.js
@@ -0,0 +1,32 @@
+  
+import React from 'react';
+
+import './PlayListtt.css';
+
+import TrackList from '../TrackList/TrackList';
+
+class PlayListtt extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleNameChange = this.handleNameChange.bind(this);
+  }
+
+  handleNameChange(event) {
+    this.props.onNameChange(event.target.value);
+  }
+
+  render() {
+    return (
+      <div className="Playlist">
+        <input onChange={this.handleNameChange} value={this.props.playlistName} />
+        <TrackList tracks={this.props.playlistTracks}
+                   isRemoval={true}
+                   onRemove={this.props.onRemove} />
+        <a className="Playlist-save" onClick={this.props.onSave}>SAVE TO SPOTIFY</a>
+      </div>
+    );
+  }
+}
+
+export default PlayListtt;
